6d2f6bc540943402a4dcee3e458b4890e011bb6f
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-08-04 17:33:11 -0700

    clear tree

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import './SignUp.css'
 import { makeStyles } from '@material-ui/core/styles'
 import SignUpForm from '../../components/SignUpForm'
 import Background from '../../assets/sponge.png'
 import Grid from '@material-ui/core/Grid'
 
 const useStyles = makeStyles({
-  mainCon: {
-    backgroundImage: `url(${Background})`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    height: 768
-  },
+  // mainCon: {
+  //   backgroundImage: `url(${Background})`,
+  //   backgroundPosition: 'center',
+  //   backgroundSize: 'cover',
+  //   backgroundRepeat: 'no-repeat',
+  //   minHeight: '100%'
+  // },
   form: {
     boxShadow: 'none',
     paddingTop: 20,
@@ -25,7 +26,7 @@ const SignUp = _ => {
   const classes = useStyles()
 
   return (
-    <div className={classes.mainCon}>
+    <div className='backgroundImg'>
       <Grid container spacing={1}>
         <Grid item xs={4} />
         <Grid item xs={8}>
